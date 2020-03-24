f6d766deebae5f31bcd7ed4bc68b92d4b4d88e9b
Dina Singatulina <dina.sing1212@gmail.com>
2020-03-24 23:17:21 +0300

    feat: add model for user roles

--- db.js
+++ db.js
@@ -2,6 +2,22 @@ const Sequelize = require('sequelize');
 
 const db = new Sequelize('postgres://postgres:1@localhost:5432/cinema');
 
+const Roles = db.define('roles', {
+    id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+    },
+    title: {
+        type: Sequelize.TEXT,
+        allowNull: false
+    }
+}
+)
+
+db.sync()
+
 module.exports = {
     db,
 }
