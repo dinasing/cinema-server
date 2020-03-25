dc5d90ca635c2c957b189361b38dee4556ff241f
Dina Singatulina <dina.sing1212@gmail.com>
2020-03-25 13:31:55 +0300

    refactor: change import and export

--- db.js
+++ db.js
@@ -1,23 +1,3 @@
-const Sequelize = require('sequelize');
+import Sequelize from 'sequelize';
 
-const db = new Sequelize('postgres://postgres:1@localhost:5432/cinema');
-
-const Roles = db.define('roles', {
-    id: {
-        type: Sequelize.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
-        allowNull: false
-    },
-    title: {
-        type: Sequelize.TEXT,
-        allowNull: false
-    }
-}
-)
-
-db.sync()
-
-module.exports = {
-    db,
-}
+export const db = new Sequelize('postgres://postgres:1@localhost:5432/cinema');
