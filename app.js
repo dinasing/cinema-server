d8fea82acbfabf58e83d586a78258c85f5c38459
Dina Singatulina <dina.sing1212@gmail.com>
2020-03-25 13:29:28 +0300

    refactor: change imports

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+import express, { json, urlencoded } from 'express';
+import { join } from 'path';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
 
-var indexRouter = require('./routes/index');
+import indexRouter from './routes/index';
 
-var app = express();
+export let app = express();
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(json());
+app.use(urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 
-module.exports = app;
+//  default app;
