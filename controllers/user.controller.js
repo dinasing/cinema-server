0574fef32ebe295a1e25e59171c1fa9bdc4386de
Dina Singatulina <dina.sing1212@gmail.com>
2020-04-01 13:55:54 +0300

    feat: add encrypting for passwords

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,45 +1,45 @@
-import { db } from '../models/index.js'
-const user = db.user;
+import { db } from "../models/index.js";
+const User = db.user;
+const bcrypt = require("bcryptjs");
 
 // Create and Save a new User
 export function create(req, res) {
-    if (!req.body.title) {
-      res.status(400).send({
-        message: "Content can not be empty!"
-      });
-      return;
-    }
-  
+  if (!req.body.email) {
+    res.status(400).send({
+      message: "Content can not be empty!"
+    });
+    return;
+  }
+
+  bcrypt.hash(req.body.password, 3, function(err, hash) {
     const user = {
-      login: req.body.login,
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
       email: req.body.email,
-      password: req.body.password,
+      password: hash,
       roleId: req.body.roleId
     };
-
     User.create(user)
       .then(record => {
         res.send(record);
       })
       .catch(err => {
         res.status(500).send({
-          message:
-            err.message || "Some error occurred while adding user."
+          message: err.message || "Some error occurred while adding user."
         });
       });
-  };
+  });
+}
 
 // Retrieve all Users from the database.
-exports.findAll = (req, res) => {
-  
-};
+exports.findAll = (req, res) => {};
 
 exports.update = (req, res) => {
-    const id = req.params.id;
+  const id = req.params.id;
 
-    User.update(req.body, {
-        where: { id: id }
-    })
+  User.update(req.body, {
+    where: { id: id }
+  })
     .then(num => {
       if (num == 1) {
         res.send({
@@ -60,11 +60,11 @@ exports.update = (req, res) => {
 
 // Delete a User with the specified id in the request
 exports.deleteOne = (req, res) => {
-    const id = req.params.id;
+  const id = req.params.id;
 
-    User.destroy({
-      where: { id: id }
-    })
+  User.destroy({
+    where: { id: id }
+  })
     .then(num => {
       if (num == 1) {
         res.send({
@@ -84,11 +84,7 @@ exports.deleteOne = (req, res) => {
 };
 
 // Delete all Users from the database.
-exports.deleteAll = (req, res) => {
-  
-};
+exports.deleteAll = (req, res) => {};
 
 // Find all published Users
-exports.findAllPublished = (req, res) => {
-  
-};
+exports.findAllPublished = (req, res) => {};
