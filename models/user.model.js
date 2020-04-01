6e35860724d2e50e6174e8ae1adde719d3d1d205
Dina Singatulina <dina.sing1212@gmail.com>
2020-04-01 13:54:41 +0300

    refactor: change model

--- models/user.model.js
+++ models/user.model.js
@@ -1,31 +1,33 @@
 export function createModel(db, Sequelize) {
-    const User = db.define("users", {
-      id: {
-        type: Sequelize.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
-      },
-      username: {
-        type: Sequelize.STRING,
-        allowNull: false,
-        unique: true,
-      },
-      email: {
-        type: Sequelize.STRING,    
-        allowNull: false,
-        unique: true,
-      },
-      password: {
-        type: Sequelize.STRING,            
-        allowNull: false
-      },
-      roleId: {        
-        type: Sequelize.INTEGER,
-        defaultValue: 1,                   
-        allowNull: false
-      }
-    });
-  
-    return User;
- 
-  };
+  const User = db.define("users", {
+    id: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    firstName: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    lastName: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    email: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      unique: true
+    },
+    password: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    roleId: {
+      type: Sequelize.INTEGER,
+      defaultValue: 1,
+      allowNull: false
+    }
+  });
+
+  return User;
+}
